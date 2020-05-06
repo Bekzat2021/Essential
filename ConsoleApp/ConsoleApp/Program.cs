0c653a821b5ee49b234c59d5feb3f8ecad7cd255
Bekzat2021 <bekzat.work@gmail.com>
2020-05-06 14:57:59 +0500

    First day

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello \"World\"");
+            Console.WriteLine("A \n B \n C \n");
+            Console.WriteLine('\x78');
+        }
+    }
+}
