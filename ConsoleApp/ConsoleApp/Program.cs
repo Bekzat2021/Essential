5a8557547739a9701e7473e75b2e6ca8d16f4c63
Bekzat2021 <bekzat.work@gmail.com>
2020-05-08 14:21:12 +0500

    Cool ternar operator

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -6,9 +6,15 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello \"World\"");
-            Console.WriteLine("A \n B \n C \n");
-            Console.WriteLine('\x78');
+            Console.Write("Enter num 1: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter num 2: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            string answer = (num1 == num2) ? $"{num1} = {num2} " : (num1 > num2) ? $"{num1} > {num2}" : $"{num1} < {num2}";
+            Console.WriteLine(answer);
         }
+
     }
 }
